fece29b090c3fc6cff6eb6e40d7750825fbfa2e4
maemay85 <hellomaemay@gmail.com>
2024-02-12 17:33:40 -0500

    added Next and Prev buttons to SinglePlroject

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import projectData from "../assets/projectData";
 
 function SingleProject() {
@@ -8,12 +8,19 @@ const project = projectData[projectId];
 
   return (
     <div id="single-project">
-      <div id="single-project-blurb">
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
+      <div id="single-project-content">
+        <div id="single-project-blurb">
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </div>
+        <div id="single-project-image">
+          <img src={`../${project.imageUrl}`} />
+        </div>
       </div>
-      <div id="single-project-image">
-        <img src={`../${project.imageUrl}`} />
+      <div id="next-prev">
+        { Number(projectId) <= 3 && Number(projectId) >1 ? <Link to={`/projects/${Number(projectId)-1}`}>{`< Prev `}</Link> : <div></div>}
+
+        { Number(projectId) >= 1 && Number(projectId) < 3 ? <Link to={`/projects/${Number(projectId)+1}`}>{` Next >`}</Link> : null}
       </div>
     </div>
   )
