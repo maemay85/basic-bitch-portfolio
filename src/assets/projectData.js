1fb0abf58e0281e1c3fca802b2a36a520426807e
maemay85 <hellomaemay@gmail.com>
2024-02-12 19:09:52 -0500

    made nav and footer fixed

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -19,6 +19,23 @@ const projectData = [
     description: 'augmentEd is a peer-to-peer music education application. Users can create and share music lessons that include text, sheet music, and a playable piano. I worked collaboratively on all aspects of this project, including developing the database schema and implementing with Sequelize ORM, and created a translation method to automatically reformat music data for the sheet music and piano libraries.',
     imageUrl: 'src/assets/thumbnail-augmentEd.png'
   },
+  { projectId: 1,
+    title: 'Law Firm of Reuben Fuller Bennett',
+    description: 'A website built in collaboration with designer Yael Krevsky with Vite React, deployed on Vercel. The site feautres an English/Español language toggle, and a serverless email form built with EmailJS.',
+    imageUrl: 'src/assets/thumbnail-rfblaw.png'
+  },
+  {
+    projectId: 2,
+    title: 'W.E.L.P. (We Effing Love Planets)',
+    description: 'We Effing Love Planets is a fullstack web application I worked as a frontend developer for. I created reusable, responsive components to render planetary bodies in CSS based on properties pulled from the database. It is an interactive educational tool to learn about our solar system.',
+    imageUrl: 'src/assets/thumbnail-welp.png'
+  },
+  {
+    projectId: 3,
+    title: 'augmentEd',
+    description: 'augmentEd is a peer-to-peer music education application. Users can create and share music lessons that include text, sheet music, and a playable piano. I worked collaboratively on all aspects of this project, including developing the database schema and implementing with Sequelize ORM, and created a translation method to automatically reformat music data for the sheet music and piano libraries.',
+    imageUrl: 'src/assets/thumbnail-augmentEd.png'
+  }
 ]
 
 export default projectData
